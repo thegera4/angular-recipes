9b2ee9f171c67102d8adc345b278a265fdaa5d0e
Gerardo Medellin <out_jmedellin@findep.com.mx>
2025-03-09 19:12:54 -0600

    feat: added form controls to the edit form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
-import { FormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component'
 import { HeaderComponent } from './header/header.component'
@@ -34,7 +34,8 @@ import { RecipeService } from './recipes/recipes.service'
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [ShoppingListService, RecipeService],
   bootstrap: [AppComponent]
